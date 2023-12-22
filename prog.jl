4a78574c6fcab15ea5d2dd9c65a745d5a13e853a
Jas sharma <91935368+CreateJas@users.noreply.github.com>
2023-12-22 18:13:39 +0530

    Add files via upload

---
+++ prog.jl
@@ -0,0 +1,12 @@
+using Flux
+
+# Simple model
+model = Dense(10, 5, σ)
+
+# Random data with Float32 type
+data = rand(Float32, 10) # 10 input features of Float32
+
+# Apply the model
+result = model(data)
+
+println("Output of the model: ", result)

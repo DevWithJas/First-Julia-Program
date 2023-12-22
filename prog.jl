a2648ffc1277d601b1ebe484916add05bb5f9384
Jas sharma <91935368+CreateJas@users.noreply.github.com>
2023-12-22 18:14:11 +0530

    Update prog.jl

--- prog.jl
+++ prog.jl
@@ -6,7 +6,7 @@ model = Dense(10, 5, σ)
 # Random data with Float32 type
 data = rand(Float32, 10) # 10 input features of Float32
 
-# Apply the model
+# Applying the model
 result = model(data)
 
 println("Output of the model: ", result)
